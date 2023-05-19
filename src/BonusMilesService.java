f416bf66560c1374134b4a06c8f1b6d3a30f0d41
Roman Boytsov <domovon@outlook.com>
2023-05-20 00:24:22 +0300

    initinal commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService{
+    public int calculate(int a) {
+        int result;
+        if (a >= 20) {
+            result = a/20;
+        } else {
+            result = 0;
+        }
+        return result;
+    }
+}
