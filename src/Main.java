2ede264c898a33fe8dc78b6fd7e56feda3eb04a6
Roman Boytsov <domovon@outlook.com>
2023-05-20 00:38:13 +0300

    change comment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 1_021;
-        int miles = service.calculate(price); // должно получиться 500
+        int miles = service.calculate(price); //Должно получиться 501
         System.out.println(miles);
     }
 }
