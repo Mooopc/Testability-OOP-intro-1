d9a2c404e17b57041b257894e1a9f2de1939d1ff
Roman Boytsov <domovon@outlook.com>
2023-05-24 21:20:54 +0300

    Reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
-public class BonusMilesService{
+public class BonusMilesService {
     public int calculate(int price) {
         int result;
         if (price >= 20) {
-            result = price/20;
+            result = price / 20;
         } else {
             result = 0;
         }
